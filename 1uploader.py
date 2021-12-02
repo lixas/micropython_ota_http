41679b128186517f0f6b8d2f9cb538ca7726e02e
Ignas Bukys <lixas666@gmail.com>
2021-12-02 08:24:42 +0200

    first working version

--- 1uploader.py
+++ 1uploader.py
@@ -4,11 +4,14 @@ import os, io, json, hashlib, time     # type: ignore
 
 FTP_ADR = "127.0.0.1"
 FTP_PRT = 21
-FTP_LOC = "OTA/"
+FTP_LOC = "/"
 FTP_USR = ""        # leave empty if annonymous
 FTP_PSW = ""
+BASE = "board/"     # with trailing slash
 
-BASE = "board/"     # no trailing slash
+# settings for json file to be used on board
+DO_BACKUP = True       # make copy of existing files for backup
+PRESERVE_ALL = False     # keep files that are already exists
 
 
 def delete_recursive(path):
@@ -16,7 +19,6 @@ def delete_recursive(path):
     files = []
     ftp.cwd(path)
     ftp.retrlines("LIST", lambda L: dirs.append(L[52:]) if L[0:1] == "d" else files.append(L[52:]))
-    # print("Path:", path, "dirs: ", dirs, "Files: ", files)
     for file in files:
         ftp.delete(file)
     for dir in dirs:
@@ -29,25 +31,19 @@ def upload_recursive(ftp, path, omit):
     for name in os.listdir(path):
         localpath = os.path.join(path, name)
         if os.path.isfile(localpath):
-            # print("STOR", name, localpath)
             ftp.storbinary('STOR ' + name, open(localpath, 'rb'))
             with open(localpath, 'rb') as f:
                 h = hashlib.sha256(f.read())
             metadata.append({"name": localpath.replace("\\", "/").replace(omit, ""), "hash": h.digest().hex()})
         elif os.path.isdir(localpath):
-            # print("MKD", name)
-
             try:
                 ftp.mkd(name)
             # ignore "directory already exists"
             except ftplib.all_errors as e:
                 if not e.args[0].startswith('550'):
                     raise
-
-            # print("CWD", name)
             ftp.cwd(name)
             upload_recursive(ftp, localpath, omit)
-            # print("CWD", "..")
             ftp.cwd("..")
 
 
@@ -59,17 +55,23 @@ else:
 
 delete_recursive(FTP_LOC)
 
+version = "{}{:02d}{:02d}{:02d}{:02d}{:02d}".format(*time.localtime()[0:6])
 metadata = []
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 upload_recursive(ftp, BASE, BASE)
 metafile = io.BytesIO()
-metafile.write(json.dumps({"files": metadata}).encode())
+metafile.write(json.dumps({
+    "version": version,
+    "do_backup": DO_BACKUP,
+    "preserve": PRESERVE_ALL,
+    "files": metadata
+}).encode())
 metafile.seek(0)  # move to beginning of file
 ftp.storbinary('STOR ' + "OTA_meta.json", metafile)
 
 versionfile = io.BytesIO()
-versionfile.write(json.dumps({"version": "{}{:02d}{:02d}{:02d}{:02d}{:02d}".format(*time.localtime()[0:6])}).encode())
+versionfile.write(json.dumps({"version": version}).encode())
 versionfile.seek(0)
 ftp.storbinary('STOR ' + "OTA_ver.json", versionfile)
 
--- board/main.py
+++ board/main.py
@@ -1,43 +1,34 @@
 import hashlib, gc, os, time
 import binascii as ubinascii
+import libs.urequests as req               # type: ignore
+import network
 
-_Platform = "MPY"        # "PC" or "MPY"
+time.sleep(3)
+# Is device microcontroller
 
-if _Platform == "PC":
-    import requests as req               # type: ignore
-elif _Platform == "MPY":
-    import libs.urequests as req               # type: ignore
+_OTA_URL_BASE = "http://192.168.1.235/"
 
-
-_UPDATE_DIR = "OTA/UPDATE"     # where to save downloaded content
-_BACKUP_DIR = "OTA/BACKUP"     # where to copy currently content
-_UPDATE_FLAG = ".OTA_PENDING"    # no trailing slash. name of file with indicates that update should be performed. Contains URL base of update
-_VERSION_FILE = ".VERSION"          # file, where
-
-# _URL_BASE = ""      # will be read from _UPDATE_FLAG where is update
+_UPDATE_DIR = "/OTA/DOWNLOADED"     # where to save downloaded content
+_BACKUP_DIR = "/OTA/BACKUP"      # where to copy currently content
+_VERSION_FILE = "/OTA/.VERSION"          # Version of current solution. if remote OTA_VER.json is bigger- need to perform update
 
 
 def file_exists(path):
     try:
         f = open(path, "r")
-        exists = True
         f.close()
+        return True
     except OSError:     # type: ignore
-        exists = False
-    return exists
-
+        return False
 
 def dir_exists(path):
     try:
-        f = os.listdir(path)
-        if f != []:
-            exists = True
+        if os.stat(path)[0] & 0x4000:  # Dir
+            return True
         else:
-            exists = False
-    except OSError:      # type: ignore
-        exists = False
-    return exists
-
+            return False
+    except OSError:     # type: ignore
+        return False
 
 def check_file_hash(file, hash=None):
     h = hashlib.sha256()
@@ -49,15 +40,14 @@ def check_file_hash(file, hash=None):
         else:
             return ubinascii.hexlify(h.digest()).decode('utf-8')
 
-def is_update_pending():       # by checking versions
-    # global _URL_BASE
-    r = req.get("{}/OTA_ver.json".format(_URL_BASE))
+def is_update_available(URL_BASE):       # by checking versions
+    r = req.get("{}/OTA_ver.json".format(URL_BASE))
     if r.status_code == 200:
         try:
             v = r.json()
             OTA_ver = int(v["version"])
         except Exception as e:          # type: ignore
-            print("{}/OTA_ver.json in not valid JSON file:".format(_URL_BASE))
+            print("{}/OTA_ver.json in not valid JSON file:".format(URL_BASE))
             print(r.content.decode("utf-8"))
             print(e)
             return False
@@ -67,74 +57,52 @@ def is_update_pending():       # by checking versions
             if OTA_ver > int(f.read()):
                 return True
     elif r.status_code in [404, 400]:
-        print("File '{1}' was not found in {0}".format(_URL_BASE, "OTA_ver.json"))
+        print("File '{1}' was not found in {0}".format(URL_BASE, "OTA_ver.json"))
     return False
 
-def download_meta():
-    global _URL_BASE
-    r = req.get("{}/OTA_meta.json".format(_URL_BASE))
+def download_meta(meta_file):
+    r = req.get(meta_file)
     if r.status_code == 200:
         return r.json()
 
-def download_file(path, parent):
-    global _URL_BASE
+def download_file(url_base, path, parent):
     # create directory structure for file
     full_path = parent
     for dir in path.split("/")[:-1]:        # check do direcotries exist
         full_path = "{}/{}".format(full_path, dir)
         if not dir_exists(full_path):
-            print(full_path, " dir does not exist. Creating")
+            # print(full_path, " dir does not exist. Creating")
             try:
                 os.mkdir(full_path)
-            except:
-                pass
+            except BaseException:
+                print("Failed to create directory: {}".format(full_path))
     del full_path
 
-    r = req.get("{}/{}".format(_URL_BASE, path))
+    r = req.get("{}/{}".format(url_base, path))
     if r.status_code == 200:
         with open("{}/{}".format(parent, path), 'wb+') as f:
                 f.write(r.content)
+    else:
+        print("Download error. HTTP error {0} for file {1}".format(r.status_code, path))
 
-def check_for_new_version(location):
-    # connect to wifi
-    r = req.get("{}/OTA_ver.json".format(location))
-    if r.status_code == 200:
-        OTA_ver = int(r.json()["version"])
-
-        # check for local version
-        local_ver = 0
-        if file_exists(_VERSION_FILE):
-            with open(_VERSION_FILE, "rb") as f:
-                local_ver = int(f.read())
-        
-        if OTA_ver > local_ver:
-            with open(_UPDATE_FLAG, "w+") as f:
-                f.write(location.encode('utf-8'))
-            return True
+def remove_recursive(source, preserve_top=False, keep_files = []):
+    for f in os.ilistdir(source):
+        if not file_exists("{}/{}".format(source, f[0])):
+            remove_recursive("/".join((source, f[0])))  # File or Dir
         else:
-            return False
+            os.remove("{}/{}".format(source, f[0]))
 
-def rm(d, preserve_top=False):  # Remove file or tree
-    try:
-        if os.stat(d)[0] & 0x4000:  # Dir
-            for f in os.ilistdir(d):
-                if f[0] not in ('.', '..'):
-                    rm("/".join((d, f[0])))  # File or Dir
-            if not preserve_top:
-                os.rmdir(d)
-        else:  # File
-            os.remove(d)
-    except:
-        print("rm of '%s' failed" % d)
+    if not preserve_top:
+        os.rmdir(source)
 
-# print(check_file_hash("boot.py", "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"))
-# Cler UPDATE catalog recursive
+def move_directory_tree(source, destination, exclude=[]):
+    for f in (os.ilistdir(source)):
+        if f[0] not in exclude:
+            os.rename("{}/{}".format(source, f[0]), "{}/{}".format(destination, f[0]))
 
-if _Platform == "PC":
-    os.chdir(os.path.dirname(os.path.realpath(__file__)))
-
-if _Platform == "MPY":
-    import network
+  
+if __name__ == "__main__":
+    # connect wifi if needed
     gc.collect()
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
@@ -145,33 +113,52 @@ if _Platform == "MPY":
             print(".", end="")
             time.sleep(0.3)
     print(".")
-    print("Connected")
-    print('network config:', sta_if.ifconfig())
+    print("Connected. Network config:", sta_if.ifconfig())
 
-# connect wifi if needed
-with open("{}".format(_UPDATE_FLAG), "rb") as f:
-    _URL_BASE = f.read().decode("utf-8")
+    if not file_exists(_VERSION_FILE):
+        with open(_VERSION_FILE, "w") as vf:
+            vf.write("0")
 
-if is_update_pending():                 # if local version older than in metdata
-    print("Update is available")
-    metafile = download_meta()          # download metadata
-    if dir_exists(_UPDATE_DIR):
-        rm(_UPDATE_DIR, True)
-    else:
-        os.mkdir(_UPDATE_DIR)
-    for file in metafile["files"]:      # read files list
-        download_file(file["name"], _UPDATE_DIR)
-        if check_file_hash("{}/{}".format(_UPDATE_DIR, file["name"]), file["hash"]):
-            print("OK", file["hash"], file["name"])
+    if is_update_available(_OTA_URL_BASE):                 # if local version older than in metadata
+        print("Update is available")
+
+        if dir_exists(_UPDATE_DIR):
+            remove_recursive(_UPDATE_DIR, True)
         else:
-            print("Hash does not match for file ", file["name"])
-            print("Local  {}".format(check_file_hash("{}/{}".format(_UPDATE_DIR, file["name"]))))
-            print("Remote {}".format(file["hash"]))
-else:
-    print("No update available")
+            os.mkdir(_UPDATE_DIR)
+
+        metafile = download_meta("{}/OTA_meta.json".format(_OTA_URL_BASE))          # download metadata from http
+        for file in metafile["files"]:      # read files list
+            download_file(_OTA_URL_BASE, file["name"], _UPDATE_DIR)
+            if check_file_hash("{}/{}".format(_UPDATE_DIR, file["name"]), file["hash"]):
+                print("Hash OK", file["hash"], file["name"])
+            else:
+                print("Hash does not match for file ", file["name"])
+
+        # make backup of all files
+        if metafile["do_backup"]:
+            if dir_exists(_BACKUP_DIR):
+                remove_recursive(_BACKUP_DIR, True)
+            else:
+                os.mkdir(_BACKUP_DIR)
+            move_directory_tree("/", _BACKUP_DIR, ["OTA"])  # move everything excluding OTA directory
 
 
+        
+        # save all content or delete except OTA
+        if metafile["preserve"]:
+            pass
 
-#         Download file
-#         Check hash
-# create update_pending file
+        # Copy new files from _UPDATE_DIR
+        move_directory_tree(_UPDATE_DIR, os.getcwd())
+
+        # update local version file
+        with open(_VERSION_FILE, "w") as ver:
+            ver.write(metafile["version"])
+        
+        # Clean up after finishing
+        remove_recursive(_UPDATE_DIR)
+
+    else:
+        print("No update available")
+
